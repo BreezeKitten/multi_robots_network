203bc67682b1e819d2afd16f064b6151144c070f
BreezeCat <jkjk010695@gmail.com>
2020-07-23 16:34:15 +0800

    GIF

---
+++ anima_multi_robot_network.py
@@ -0,0 +1,369 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 11:14:57 2020
+
+@author: BreezeCat
+"""
+import time as TIME
+import tensorflow.compat.v1 as tf
+import json
+import random
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import datetime
+import copy
+import file_manger
+import state_load as SL
+import os
+import Agent
+import Network
+
+tf.reset_default_graph()
+color_list = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+
+####
+#Common parameter
+####
+PI = math.pi
+resX = 0.1 # resolution of X
+resY = 0.1 # resolution of Y
+resTH = PI/15
+LOG_DIR = 'logs/Multi_test'
+SOME_TAG = '_test4'
+
+####
+#Reward
+####
+Arrived_reward = 1
+Time_out_penalty = -0.25
+Collision_high_penalty = -0.5
+Collision_low_penalty = -1
+Collision_equ_penalty = -0.75
+
+
+'''
+DL Parameter
+'''
+training_eposide_num = 5000 #100000 
+training_num = 1500 #3000
+test_num = 1
+two_robot_Network_Path = '2_robot_network/gamma09_95_0429/test.ckpt'
+
+'''
+Motion Parameter
+'''
+deltaT = 0.1            #unit:s
+V_max = 3               #m/s
+W_max = 2               #rad/s
+linear_acc_max = 10     #m/s^2
+angular_acc_max = 7     #rad/s^2
+size_min = 0.1          #unit:m
+x_upper_bound = 5       #unit:m
+x_lower_bound = -5      #unit:m
+y_upper_bound = 5       #unit:m
+y_lower_bound = -5      #unit:m
+TIME_OUT_FACTOR = 4
+
+
+agnet2_motion = 'Greedy'
+agnet3_motion = 'Greedy'
+agnet4_motion = 'Greedy'
+RL_eposide_num = 100
+RL_epsilon = 0
+gamma = 0.9
+
+
+
+def Build_network(session, robot_num):
+    Network_set = []
+    for i in range(robot_num - 1):
+        Network_set.append(Network.Two_robot_network(str(10+i+1)))
+    for item in Network_set:
+        item.restore_parameter(session, two_robot_Network_Path)
+    with tf.name_scope('Smallest_value'):
+        smaller_value_list = [Network_set[0].value]
+        for i in range(len(Network_set)-1):
+            smaller_value_list.append(tf.minimum(smaller_value_list[i], Network_set[i+1].value))
+        smallest_value = smaller_value_list[-1]
+    return smallest_value, Network_set
+
+
+
+def Calculate_distance(x1, y1, x2, y2):
+    return np.sqrt(math.pow( (x1-x2) , 2) + math.pow( (y1-y2) , 2))
+
+def Check_Collision(agent1, agent2):
+    distance = Calculate_distance(agent1.state.Px, agent1.state.Py, agent2.state.Px, agent2.state.Py)
+    if (distance <= (agent1.state.r + agent2.state.r)):
+        return True
+    else:
+        return False
+
+
+def Check_Goal(agent, position_tolerance, orientation_tolerance):    
+    position_error = Calculate_distance(agent.state.Px, agent.state.Py, agent.gx, agent.gy)
+    orientation_error = abs(agent.state.Pth - agent.gth)
+    if (position_error < position_tolerance) and (orientation_error < orientation_tolerance):
+        return True
+    else:
+        return False
+
+def Random_Agent(name):
+    Px = random.random()*(x_upper_bound - x_lower_bound) + x_lower_bound
+    Py = random.random()*(y_upper_bound - y_lower_bound) + y_lower_bound
+    Pth = random.random()*2*PI 
+    V = 0 #(random.random() - 0.5) * V_max
+    W = 0 #(random.random() - 0.5) * W_max
+    r = random.random() + size_min
+    gx = random.random()*(x_upper_bound - x_lower_bound) + x_lower_bound
+    gy = random.random()*(y_upper_bound - y_lower_bound) + y_lower_bound
+    gth = random.random()*2*PI 
+    rank = random.randint(1,3)
+    return Agent.Agent(name, Px, Py, Pth, V, W, r, gx, gy, gth, rank, mode = 'Greedy')
+
+def Predict_action_value(main_agent, Agent_Set, V_pred, W_pred):
+    State_list = []
+    for agent in Agent_Set:
+        if main_agent.name != agent.name:
+            pred_state = main_agent.Predit_state(V_pred, W_pred, dt = deltaT)
+            obs_state = agent.Relative_observed_state(pred_state.Px, pred_state.Py, pred_state.Pth)
+            obs_gx, obs_gy, obs_gth = main_agent.Relative_observed_goal(pred_state.Px, pred_state.Py, pred_state.Pth)
+            m11, m12, m13 = 0, 0, 0
+            if main_agent.rank > agent.rank:   
+                m11 = 1
+            elif main_agent.rank < agent.rank:   
+                m13 = 1
+            else:   
+                m12 = 1
+            State_list.append([[V_pred, W_pred, main_agent.state.r, obs_gx, obs_gy, obs_gth, V_max, m11, m12, m13, obs_state.x, obs_state.y, obs_state.Vx, obs_state.Vy, obs_state.r]])
+            
+    if len(State_list) == len(Network_list):
+        state_dict = {}
+        for i in range(len(State_list)):
+            state_dict[Network_list[i].state] = State_list[i]
+    else:
+        print('robot num error')
+        return 0
+    value_matrix = sess.run(Value, feed_dict = state_dict)
+    
+    R = 0
+    
+    main_agent_pred = Agent.Agent('Pred', pred_state.Px, pred_state.Py, pred_state.Pth, pred_state.V, pred_state.W, pred_state.r, main_agent.gx, main_agent.gy, main_agent.gth, main_agent.rank)
+    if Check_Goal(main_agent_pred, Calculate_distance(resX, resY, 0, 0), resTH):
+        R = Arrived_reward
+    for item in Agent_Set:
+        if main_agent.name != item.name:
+            if Check_Collision(main_agent, item):
+                if main_agent.rank > item.rank:
+                    R = Collision_high_penalty
+                elif main_agent.rank < item.rank:   
+                    R = Collision_low_penalty
+                else:   
+                    R = Collision_equ_penalty
+                break
+    action_value = R + value_matrix[0][0]
+                
+    return action_value
+
+
+def Choose_action_from_Network(main_agent, Agent_Set, epsilon):
+    dice = random.random()
+    action_value_max = -999999   
+    if dice < epsilon:
+        linear_acc = -linear_acc_max + random.random() * 2 * linear_acc_max
+        angular_acc = -angular_acc_max + random.random() * 2 * angular_acc_max
+        V_pred = np.clip(main_agent.state.V + linear_acc * deltaT, -V_max, V_max)
+        W_pred = np.clip(main_agent.state.W + angular_acc * deltaT, -W_max, W_max)
+    else:
+        linear_acc_set = np.arange(-linear_acc_max, linear_acc_max, 1)
+        angular_acc_set = np.arange(-angular_acc_max, angular_acc_max, 1)
+        for linear_acc in linear_acc_set:
+            V_pred = np.clip(main_agent.state.V + linear_acc * deltaT, -V_max, V_max)
+            for angular_acc in angular_acc_set:
+                W_pred = np.clip(main_agent.state.W + angular_acc * deltaT, -W_max, W_max)
+                action_value = Predict_action_value(main_agent, Agent_Set, V_pred, W_pred)
+                if action_value > action_value_max:
+                    action_value_max = action_value
+                    action_pair = [V_pred, W_pred]                    
+        V_pred = action_pair[0]
+        W_pred = action_pair[1]
+        #print(action_value_max)
+    return V_pred, W_pred
+
+
+def Choose_action(main_agent, Agent_Set):
+    if main_agent.mode == 'Static':
+        V_next, W_next = 0, 0
+    if main_agent.mode == 'Random':
+        V_next = main_agent.state.V + random.random() - 0.5
+        W_next = main_agent.state.W + random.random() - 0.5
+    if main_agent.mode == 'Greedy':
+        V_next, W_next = Choose_action_from_Network(main_agent, Agent_Set, 0)
+        
+    return V_next, W_next
+
+def Show_Path(Agent_Set, result, save_path):
+    plt.close('all')
+    plt.figure(figsize=(12,12))
+    ax = plt.gca()
+    ax.cla()    
+    ax.set_xlim((x_lower_bound,x_upper_bound))     #上下限
+    ax.set_ylim((x_lower_bound,x_upper_bound))
+    plt.xlabel('X(m)')
+    plt.ylabel('Y(m)')
+    color_count = 0
+    for agent in Agent_Set:
+        agent.Plot_Path(ax = ax, color = color_list[color_count%len(color_list)])
+        agent.Plot_goal(ax = ax, color = color_list[color_count%len(color_list)])
+        color_count += 1
+    NOW = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+    plt.savefig(save_path +'/image/'+ NOW + result +'.png')
+    return
+
+
+def Show_GIF_Path(Agent_Set, result, save_path):
+    plt.close('all')
+    plt.figure(figsize=(12,12))
+    ax = plt.gca()
+    ax.cla()    
+    ax.set_xlim((x_lower_bound,x_upper_bound))     #上下限
+    ax.set_ylim((x_lower_bound,x_upper_bound))
+    plt.xlabel('X(m)')
+    plt.ylabel('Y(m)')
+    color_count = 0
+    for agent in Agent_Set:
+        agent.Plot_Path(ax = ax, color = color_list[color_count%len(color_list)])
+        agent.Plot_goal(ax = ax, color = color_list[color_count%len(color_list)])
+        color_count += 1
+    plt.savefig(save_path + '/' + result +'.png')
+    return
+
+def RL_process(robot_num, eposide_num, epsilon, RL_SAVE_PATH):      
+    for eposide in range(eposide_num):
+        TIME.sleep(1)
+        NOW = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+        GIF_path = RL_SAVE_PATH + '/image/' + NOW
+        os.makedirs(GIF_path)
+        
+        if eposide%20 == 0:
+            print(eposide)
+        Main_Agent = Random_Agent('Main')
+        Agent_Set = [Main_Agent]
+        for i in range(robot_num-1):
+            Agent_Set.append(Random_Agent(str(i+2)))
+       
+        
+        
+        time = 0
+        result = 'Finish'
+        
+        Collision_Flag = False
+        Goal_dist_Flag = False
+        for item in Agent_Set:
+            for item2 in Agent_Set:
+                if item.name != item2.name:
+                    Collision_Flag = Collision_Flag or Check_Collision(item, item2)
+                    Goal_dist_Flag = Goal_dist_Flag or Calculate_distance(item.gx, item.gy, item2.gx, item2.gy) < (item.state.r + item2.state.r)
+                if Collision_Flag or Goal_dist_Flag:
+                    break
+            if Collision_Flag or Goal_dist_Flag:
+                break
+        if Collision_Flag or Goal_dist_Flag:
+            continue
+
+        if Check_Goal(Main_Agent, Calculate_distance(resX, resY, 0, 0), resTH):
+            continue
+
+            
+        TIME_OUT = Calculate_distance(Main_Agent.state.Px, Main_Agent.state.Py, Main_Agent.gx, Main_Agent.gy) * TIME_OUT_FACTOR
+
+        '''
+        agent1_init_state = [agent1.Px, agent1.Py, agent1.Pth, agent1.V, agent1.W, agent1.r, agent1.gx, agent1.gy, agent1.gth, agent1.rank, agent1.m11, agent1.m12, agent1.m13, V_max]
+        agent2_init_state = [agent2.Px, agent2.Py, agent2.Pth, agent2.V, agent2.W, agent2.r, agent2.gx, agent2.gy, agent2.gth, agent2.rank, agent2.m11, agent2.m12, agent2.m13, V_max]
+        agent3_init_state = [agent3.Px, agent3.Py, agent3.Pth, agent3.V, agent3.W, agent3.r, agent3.gx, agent3.gy, agent3.gth, agent3.rank, agent3.m11, agent3.m12, agent3.m13, V_max]
+        agent4_init_state = [agent4.Px, agent4.Py, agent4.Pth, agent4.V, agent4.W, agent4.r, agent4.gx, agent4.gy, agent4.gth, agent4.rank, agent4.m11, agent4.m12, agent4.m13, V_max]
+        '''
+        goal_flag = True
+        for agent in Agent_Set:
+            goal_flag = goal_flag and Check_Goal(agent, Calculate_distance(resX, resY, 0, 0), resTH)
+        while(not goal_flag):
+            if time > TIME_OUT:
+                result = 'TIME_OUT'
+                break
+            
+            for item in Agent_Set:
+                if Main_Agent.name != item.name:
+                    if Check_Collision(Main_Agent, item):
+                        if Main_Agent.rank > item.rank:
+                            result = 'Collision_high'
+                        elif Main_Agent.rank < item.rank:   
+                            result = 'Collision_low'
+                        else:   
+                            result = 'Collision_equal'
+                        break
+            if result != 'Finish':
+                break
+            else:
+                for agent in Agent_Set:
+                    if Check_Goal(agent, Calculate_distance(resX, resY, 0, 0), resTH):
+                        V_next, W_next = 0, 0                    
+                    else:
+                        V_next, W_next = Choose_action(agent, Agent_Set)
+                    agent.Set_V_W(V_next, W_next)
+                    
+                for agent in Agent_Set:
+                    agent.Update_state(dt = deltaT)
+            
+            Show_GIF_Path(Agent_Set, str(int(10*time)), GIF_path)
+            time = time + deltaT
+            goal_flag = True
+            for agent in Agent_Set:
+                goal_flag = goal_flag and Check_Goal(agent, Calculate_distance(resX, resY, 0, 0), resTH)
+
+        '''    
+        lines =  str(agent1_init_state) + ';' + str(agent2_init_state) + ';' + str(agent3_init_state) + ';' + str(agent4_init_state) + '\n'   
+        if result == 'Finish':
+            Path = Calculate_value(Path, 1, time)
+            f = open(RL_SAVE_PATH + '/Finish.json', 'a+')
+            f.writelines(lines)
+            f.close()
+        elif result == 'TIME_OUT':
+            Path = Calculate_value(Path, -1, time)
+            f = open(RL_SAVE_PATH + '/TimeOut.json', 'a+')
+            f.writelines(lines)
+            f.close()
+        elif result == 'Collision_high':
+            Path = Calculate_value(Path, Collision_high_penalty, time)
+            f = open(RL_SAVE_PATH + '/Collision.json', 'a+')
+            f.writelines(lines)
+            f.close()
+        elif result == 'Collision_low':
+            Path = Calculate_value(Path, Collision_low_penalty, time)
+            f = open(RL_SAVE_PATH + '/Collision.json', 'a+')
+            f.writelines(lines)
+            f.close()
+        elif result == 'Collision_equal':
+            Path = Calculate_value(Path, Collision_equ_penalty, time)
+            f = open(RL_SAVE_PATH + '/Collision.json', 'a+')
+            f.writelines(lines)
+            f.close()
+        else:
+            print('Unexpected result: ', result)
+            f = open(RL_SAVE_PATH + '/Unexpected.json', 'a+')
+            f.writelines(lines)
+            f.close()
+            
+        Record_data(Path, RL_SAVE_PATH +'/RL_Path.json')
+        '''
+        
+        Show_Path(Agent_Set, result, RL_SAVE_PATH)
+
+
+    return
+
+
+if __name__ == '__main__':
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.20) 
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+    Value, Network_list = Build_network(sess, 5)
